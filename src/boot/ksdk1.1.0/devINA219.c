1de1245f22c51160f9affbebb6675f37c94278ce
TimPearson24 <56513469+TimPearson24@users.noreply.github.com>
2019-11-19 14:42:31 +0000

    c code framework (empty functions)

--- src/boot/ksdk1.1.0/devINA219.c
+++ src/boot/ksdk1.1.0/devINA219.c
@@ -1,1 +1,49 @@
-/* File for C code*/
+/* C code for INA219*/
+
+#include <stdlib.h>
+
+#include "fsl_misc_utilities.h"
+#include "fsl_device_registers.h"
+#include "fsl_i2c_master_driver.h"
+#include "fsl_spi_master_driver.h"
+#include "fsl_rtc_driver.h"
+#include "fsl_clock_manager.h"
+#include "fsl_power_manager.h"
+#include "fsl_mcglite_hal.h"
+#include "fsl_port_hal.h"
+
+#include "gpio_pins.h"
+#include "SEGGER_RTT.h"
+#include "warp.h"
+
+
+extern volatile WarpI2CDeviceState	deviceINA219State;
+extern volatile uint32_t		gWarpI2cBaudRateKbps;
+extern volatile uint32_t		gWarpI2cTimeoutMilliseconds;
+extern volatile uint32_t		gWarpSupplySettlingDelayMilliseconds;
+
+
+void
+initINA219(const uint8_t i2cAddress, WarpI2CDeviceState volatile *  deviceStatePointer)
+{
+	deviceStatePointer->i2cAddress	= i2cAddress;
+	deviceStatePointer->signalType	= (kWarpTypeMaskCurrent);
+
+	return;
+}
+
+WarpStatus
+writeSensorRegisterINA219(uint8_t deviceRegister, uint16_t payload, uint16_t menuI2cPullupValue){
+}
+
+WarpStatus	
+configureSensorINA219(uint8_t payloadConfigReg, uint8_t payloadFrameRateReg, uint16_t menuI2cPullupValue){
+}
+
+WarpStatus
+readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes){
+}
+
+void	
+printSensorDataINA219(bool hexModeFlag){
+}
