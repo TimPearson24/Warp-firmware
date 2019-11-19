5c67d713b7d58ff25b5f7060b96e0c1ef8975e98
TimPearson24 <56513469+TimPearson24@users.noreply.github.com>
2019-11-19 14:26:35 +0000

    header file attempt 1

--- src/boot/ksdk1.1.0/devINA219.h
+++ src/boot/ksdk1.1.0/devINA219.h
@@ -1,1 +1,16 @@
-/* Create header file for INA219 */
+/* Header file for INA219 */
+
+#ifndef WARP_BUILD_ENABLE_DEVINA219
+#define WARP_BUILD_ENABLE_DEVINA219
+#endif
+
+void		initINA219(const uint8_t i2cAddress, WarpI2CDeviceState volatile *  deviceStatePointer);
+WarpStatus	readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes);
+WarpStatus	writeSensorRegisterINA219(uint8_t deviceRegister, uint16_t payload, uint16_t menuI2cPullupValue);
+WarpStatus	configureSensorINA219(uint8_t payloadConfigReg, uint8_t payloadFrameRateReg, uint16_t menuI2cPullupValue);
+WarpStatus	readSensorSignalINA219(WarpTypeMask signal,
+					WarpSignalPrecision precision,
+					WarpSignalAccuracy accuracy,
+					WarpSignalReliability reliability,
+					WarpSignalNoise noise);
+void		printSensorDataINA219(bool hexModeFlag);
