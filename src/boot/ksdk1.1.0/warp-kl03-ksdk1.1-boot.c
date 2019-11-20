7a018d06fc04b1c6ecb11071308e39d6a42ff15e
TimPearson24 <56513469+TimPearson24@users.noreply.github.com>
2019-11-20 12:20:58 +0000

    tried re-adding #

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -1469,6 +1469,18 @@ main(void)
 
 		switch (key)
 		{
+			//current measurement
+
+			case '#':
+			{
+				SEGGER_RTT_printf(0, "\r\tCow goes nayy \n");
+				enableI2Cpins(menuI2cPullupValue);
+				printSensorDataINA219();
+
+				break;
+			}
+				
+				
 			/*
 			 *	Activate low-power modes in all sensors.
 			 */
