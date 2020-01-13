963620d20d6f1fcd48b6e099e6798872ea310d4e
TimPearson24 <56513469+TimPearson24@users.noreply.github.com>
2020-01-13 18:35:32 +0000

    removed hashed out code

--- src/boot/ksdk1.1.0/devINA219.h
+++ src/boot/ksdk1.1.0/devINA219.h
@@ -4,19 +4,6 @@
 #define WARP_BUILD_ENABLE_DEVINA219
 #endif
 
-/*
-void		initINA219(const uint8_t i2cAddress, WarpI2CDeviceState volatile *  deviceStatePointer);
-WarpStatus	readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes);
-WarpStatus	writeSensorRegisterINA219(uint8_t deviceRegister, uint16_t payload, uint16_t menuI2cPullupValue);
-WarpStatus	configureSensorINA219(uint8_t payloadConfigReg, uint8_t payloadFrameRateReg, uint16_t menuI2cPullupValue);
-WarpStatus	readSensorSignalINA219(WarpTypeMask signal,
-					WarpSignalPrecision precision,
-					WarpSignalAccuracy accuracy,
-					WarpSignalReliability reliability,
-					WarpSignalNoise noise);
-void		printSensorDataINA219(bool hexModeFlag);
-*/
-
 void		initINA219(const uint8_t i2cAddress, WarpI2CDeviceState volatile *  deviceStatePointer);
 WarpStatus	readSensorRegisterINA219(int numberOfBytes);
 WarpStatus	writeSensorRegisterINA219(uint8_t deviceRegister,
