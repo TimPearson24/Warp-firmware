c3d0a5ecbfe92cd676dc211aecb1a6e3681b39ec
TimPearson24 <56513469+TimPearson24@users.noreply.github.com>
2020-01-13 18:36:30 +0000

    removed hashed out code

--- src/boot/ksdk1.1.0/devINA219.c
+++ src/boot/ksdk1.1.0/devINA219.c
@@ -35,7 +35,6 @@ initINA219(const uint8_t i2cAddress, WarpI2CDeviceState volatile *  deviceStateP
 WarpStatus
 readSensorRegisterINA219(int numberOfBytes)
 {
-//	uint8_t		cmdBuf[1];
 	i2c_status_t	status;
 
 	USED(numberOfBytes);
@@ -47,7 +46,6 @@ readSensorRegisterINA219(int numberOfBytes)
 	};
 
 
-	//cmdBuf[0] = deviceRegister;
 	// From the data sheet, read address is set by a pointer on the current sensor chip and
 	// must be set by a write command. The device address is therefore not sent during the read
 	// command and so cmdBuf size should be 0
@@ -129,55 +127,3 @@ printSensorDataINA219()
         SEGGER_RTT_printf(0, "\nShunt Voltage Reading: %d, giving Shunt Voltage: %duV, giving Current: %duA", readSensorRegisterValueCombined,readSensorRegisterValueCombined*10, readSensorRegisterValueCombined*100);
 }
 
-
-
-
-/*
-#include <stdlib.h>
-
-#include "fsl_misc_utilities.h"
-#include "fsl_device_registers.h"
-#include "fsl_i2c_master_driver.h"
-#include "fsl_spi_master_driver.h"
-#include "fsl_rtc_driver.h"
-#include "fsl_clock_manager.h"
-#include "fsl_power_manager.h"
-#include "fsl_mcglite_hal.h"
-#include "fsl_port_hal.h"
-
-#include "gpio_pins.h"
-#include "SEGGER_RTT.h"
-#include "warp.h"
-
-
-extern volatile WarpI2CDeviceState	deviceINA219State;
-extern volatile uint32_t		gWarpI2cBaudRateKbps;
-extern volatile uint32_t		gWarpI2cTimeoutMilliseconds;
-extern volatile uint32_t		gWarpSupplySettlingDelayMilliseconds;
-
-
-void
-initINA219(const uint8_t i2cAddress, WarpI2CDeviceState volatile *  deviceStatePointer)
-{
-	deviceStatePointer->i2cAddress	= i2cAddress;
-	deviceStatePointer->signalType	= (kWarpTypeMaskCurrent);
-
-	return;
-}
-
-WarpStatus
-writeSensorRegisterINA219(uint8_t deviceRegister, uint16_t payload, uint16_t menuI2cPullupValue){
-}
-
-WarpStatus	
-configureSensorINA219(uint8_t payloadConfigReg, uint8_t payloadFrameRateReg, uint16_t menuI2cPullupValue){
-}
-
-WarpStatus
-readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes){
-}
-
-void	
-printSensorDataINA219(bool hexModeFlag){
-}
-*/
